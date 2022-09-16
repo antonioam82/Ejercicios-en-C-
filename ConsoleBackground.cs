c0ba6292e2ac6d8e0619df86bfa7f6e076568902
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-09-16 17:22:37 +0200

    Create ConsoleBackground.cs

---
+++ ConsoleBackground.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Clear();
+            Console.WriteLine("This is green :)");
+        }
+    }
+}
