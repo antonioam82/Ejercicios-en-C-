fadd131dabfcec892925fb926d53ca7a6680a539
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-10-29 19:40:37 +0200

    Update adivinaNumero.cs

--- adivinaNumero.cs
+++ adivinaNumero.cs
@@ -8,35 +8,46 @@ class Program
         int aleatorio = numero.Next(0, 100);
         int minumero;
         int intentos = 0;
+        String continuar = "Y";
 
-        Console.WriteLine("Introduce un número entre 0 y 100:");
+
+        do
+        {
+            Console.WriteLine("Introduce un número entre 0 y 100:");
 
 
-        do{
-            intentos++;
-            try
+            do
             {
-                minumero = int.Parse(Console.ReadLine());
-            }
-            catch(Exception ex) when (ex.GetType() != typeof(FormatException))
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("Hubo un problema con la información introducida (el valor actual es 0)");
-                minumero = 0;
-            }
-            catch(FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("Introduzca valor nemrico entre 0 y 100");
-                minumero = 0;
-            }
-           
+                intentos++;
+                try
+                {
+                    minumero = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex) when (ex.GetType() != typeof(FormatException))
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Hubo un problema con la información introducida (el valor actual es 0)");
+                    minumero = 0;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Introduzca valor nemrico entre 0 y 100");
+                    minumero = 0;
+                }
 
-            if (minumero > aleatorio) Console.WriteLine("El número es más bajo");
-            if (minumero < aleatorio) Console.WriteLine("El número es más alto");
 
-        } while (aleatorio != minumero);
+                if (minumero > aleatorio) Console.WriteLine("El número es más bajo");
+                if (minumero < aleatorio) Console.WriteLine("El número es más alto");
+
+            } while (aleatorio != minumero);
             Console.WriteLine($"Lo has conseguido en {intentos} intentos");
+            intentos = 0;
+            aleatorio = numero.Next(0, 100);
+            Console.WriteLine("¿Desea continuar?:");
+            continuar = Console.ReadLine();
+
+        } while (continuar == "Y");
     }
 }
 
